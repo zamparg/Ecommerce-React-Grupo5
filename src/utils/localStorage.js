a051fba0d28bd57bb53947dc06dfee29c3ff72e9
Ivan Gonzalo Tapia <108291957+IvanGonzal0@users.noreply.github.com>
2023-11-30 08:52:56 -0300

    Refactor de directorios

--- src/utils/localStorage.js
+++
@@ -1,7 +0,0 @@
-export const setLocalStorage = (key, data) => {
-  localStorage.setItem(key, JSON.stringify(data))
-}
-
-export const getLocalStorage = (key) => {
-  return JSON.parse(localStorage.getItem(key))
-}
